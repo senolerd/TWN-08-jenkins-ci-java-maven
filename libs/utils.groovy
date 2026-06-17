6cdcbe4566ba9e569fc887a347d7e58349bc88ca
Senol Erdogan <senol@mac.local>
2026-06-17 14:42:55 -0500

    Version update test #4 Interpolation fix

--- libs/utils.groovy
+++ libs/utils.groovy
@@ -6,10 +6,10 @@ def getProjectVersion() {
 }
 
 def incrementVersion() {
-    sh '''
+    sh """
         podman run --rm -v jenkins_home:/app -w /app/workspace/$JOB_NAME $MAVEN_IMG mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.nextMinorVersion}.0.SNAPSHOT versions:commit
-        # podman run --rm -v jenkins_home:/app -w /app/workspace/$JOB_NAME $MAVEN_IMG mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0.SNAPSHOT versions:commit
-    '''
+        # podman run --rm -v jenkins_home:/app -w /app/workspace/$JOB_NAME $MAVEN_IMG mvn build-helper:parse-version versions:set -DnewVersion='\$'{parsedVersion.majorVersion}.'\$'{parsedVersion.nextMinorVersion}.0.SNAPSHOT versions:commit
+    """
 }
 
 
